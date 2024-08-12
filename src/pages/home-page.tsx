cc57ac3bf1e7df3b53f40ce1d3ed007d2c551e30
Rahim Daud <rahimdaud246@gmail.com>
2024-08-12 02:32:49 +0000

    chore: Update homepage heading styles and font sizes

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -12,24 +12,25 @@ const Hero: React.FC = () => {
       <div className="absolute inset-0 bg-black opacity-50"></div>
       <div className="relative text-left text-white max-w-4xl">
         <div className="mb-8">
-          <p
-            className="font-bold text-sm leading-tight mb-4"
+          <h1
+            className="font-black text-[4em] leading-tight mb-4"
             style={{ fontFamily: "Abhaya Libre" }}
           >
             {home.brand}
-          </p>
+          </h1>
+
+          <h3
+            className="text-2xl font-bold"
+            style={{ fontFamily: "Abhaya Libre" }}
+          >
+            {home.slogan}
+          </h3>
           <p
-            className="mt-6 mb-6 text-2xl"
+            className="mt-6 mb-6 text-lg opacity-50"
             style={{ fontFamily: "Abhaya Libre" }}
           >
             {home.tagline}
           </p>
-          <h1
-            className="text-4xl font-bold"
-            style={{ fontFamily: "Abhaya Libre" }}
-          >
-            {home.slogan}
-          </h1>
         </div>
         <LeadAttorneys />
         <button className="mt-8 px-8 py-2 bg-[#b4816b60] hover:bg-[#b37b63] text-sm rounded-md">
