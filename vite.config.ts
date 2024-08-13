21cb3e3d29abfd87fd1e388b1f6fe62516a31c64
Rahim Daud <rahimdaud246@gmail.com>
2024-08-13 23:18:46 +0000

    chore: Remove unused ViteFaviconsPlugin configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ViteFaviconsPlugin } from 'vite-plugin-favicon';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
-  ViteFaviconsPlugin({
-    logo: './src/assets/logo.png',
-    favicons: {
-      appName: 'Alliance Law Firm',
-      appDescription: 'Alliance Law Firm',
-      developerName: 'piccolo',
-      developerURL: null, // prevent retrieving from the nearest package.json
-      background: '#ddd',
-      theme_color: '#333',
-      icons: {
-        coast: false,
-        yandex: false
-      }
-    }
-  })
+
   ],
 
 })
