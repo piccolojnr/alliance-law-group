e35b4870196d23d0d9ca069587520bbc7ee03bc4
Rahim Daud <rahimdaud246@gmail.com>
2024-08-14 00:28:15 +0000

    Update attorney experience from 60 years to 40 years

--- src/_data/team.ts
+++ src/_data/team.ts
@@ -2,242 +2,222 @@ export const teamMembers = [
     {
         name: "Arianne L. Palmer",
         role: "Attorney",
-        image: "/images/placeholder1.jpg",
-        email: "arianlpalma@gmail.com",
+        image: "/images/female_placeholder_1.jpg",
+        email: "arianne.palmer@gmail.com",
         socials: [
-            {
-                icon: "F",
-                url: "https://www.facebook.com",
-            },
-            {
-                icon: "T",
-                url: "https://www.twitter.com",
-            },
-            {
-                icon: "I",
-                url: "https://www.instagram.com",
-            },
-        ],
-    },
-    {
-        name: "Darren L’Morrison",
-        role: "Attorney",
-        image: "/images/placeholder2.jpg",
-        email: "darrn@gmail.com",
-        socials: [
-            {
-                icon: "F",
-                url: "https://www.facebook.com",
-            },
-            {
-                icon: "T",
-                url: "https://www.twitter.com",
-            },
-            {
-                icon: "I",
-                url: "https://www.instagram.com",
-            },
-        ],
-    },
-    {
-        name: "Thomas L’Palmer",
-        role: "Attorney",
-        image: "/images/placeholder3.jpg",
-        email: "tomas@gmail.com",
-        socials: [
-            {
-                icon: "F",
-                url: "https://www.facebook.com",
-            },
-            {
-                icon: "T",
-                url: "https://www.twitter.com",
-            },
-            {
-                icon: "I",
-                url: "https://www.instagram.com",
-            },
-        ],
-    },
-    {
-        name: "Rebecca S. White",
-        role: "Senior Partner",
-        image: "/images/placeholder4.jpg",
-        email: "rebecca.white@gmail.com",
-        socials: [
-            {
-                icon: "L",
-                url: "https://www.linkedin.com",
-            },
-            {
-                icon: "T",
-                url: "https://www.twitter.com",
-            },
-            {
-                icon: "I",
-                url: "https://www.instagram.com",
-            },
-        ],
-    },
-    {
-        name: "James P. Lawrence",
-        role: "Litigation Specialist",
-        image: "/images/placeholder5.jpg",
-        email: "james.lawrence@gmail.com",
-        socials: [
-            {
-                icon: "F",
-                url: "https://www.facebook.com",
-            },
-            {
-                icon: "T",
-                url: "https://www.twitter.com",
-            },
-            {
-                icon: "I",
-                url: "https://www.instagram.com",
-            },
-        ],
-    },
-    {
-        name: "Natalie A. Graham",
-        role: "Corporate Lawyer",
-        image: "/images/placeholder6.jpg",
-        email: "natalie.graham@gmail.com",
-        socials: [
-            {
-                icon: "L",
-                url: "https://www.linkedin.com",
-            },
-            {
-                icon: "F",
-                url: "https://www.facebook.com",
-            },
-            {
-                icon: "T",
-                url: "https://www.twitter.com",
-            },
-        ],
-    },
-    {
-        name: "Michael K. Robertson",
-        role: "Legal Advisor",
-        image: "/images/placeholder7.jpg",
-        email: "michael.robertson@gmail.com",
-        socials: [
-            {
-                icon: "T",
-                url: "https://www.twitter.com",
-            },
-            {
-                icon: "I",
-                url: "https://www.instagram.com",
-            },
-            {
-                icon: "L",
-                url: "https://www.linkedin.com",
-            },
-        ],
-    },
-    {
-        name: "Cynthia L. Bennett",
-        role: "Family Law Specialist",
-        image: "/images/placeholder8.jpg",
-        email: "cynthia.bennett@gmail.com",
-        socials: [
-            {
-                icon: "F",
-                url: "https://www.facebook.com",
-            },
-            {
-                icon: "I",
-                url: "https://www.instagram.com",
-            },
-            {
-                icon: "T",
-                url: "https://www.twitter.com",
-            },
+            { icon: "F", url: "https://www.facebook.com" },
+            { icon: "T", url: "https://www.twitter.com" },
+            { icon: "I", url: "https://www.instagram.com" },
         ],
     },
     {
         name: "John D. Smith",
         role: "Paralegal",
-        image: "/images/placeholder9.jpg",
-        email: "john.smith@gmail.com",
+        image: "/images/male_placeholder_1.jpg",
+        email: "john.smith@yahoo.com",
         socials: [
-            {
-                icon: "F",
-                url: "https://www.facebook.com",
-            },
-            {
-                icon: "L",
-                url: "https://www.linkedin.com",
-            },
-            {
-                icon: "T",
-                url: "https://www.twitter.com",
-            },
+            { icon: "F", url: "https://www.facebook.com" },
+            { icon: "T", url: "https://www.twitter.com" },
+            { icon: "I", url: "https://www.instagram.com" },
         ],
     },
     {
-        name: "Emma W. Collins",
-        role: "Intern",
-        image: "/images/placeholder10.jpg",
-        email: "emma.collins@gmail.com",
-        socials: [
-            {
-                icon: "I",
-                url: "https://www.instagram.com",
-            },
-            {
-                icon: "T",
-                url: "https://www.twitter.com",
-            },
-            {
-                icon: "L",
-                url: "https://www.linkedin.com",
-            },
-        ],
-    },
-    {
-        name: "David L. Johnson",
-        role: "Legal Consultant",
-        image: "/images/placeholder11.jpg",
-        email: "david.johnson@gmail.com",
-        socials: [
-            {
-                icon: "F",
-                url: "https://www.facebook.com",
-            },
-            {
-                icon: "T",
-                url: "https://www.twitter.com",
-            },
-            {
-                icon: "I",
-                url: "https://www.instagram.com",
-            },
-        ],
-    },
-    {
-        name: "Mary J. Davis",
+        name: "Emily R. Johnson",
         role: "Legal Assistant",
-        image: "/images/placeholder12.jpg",
-        email: "mary.davis@gmail.com",
+        image: "/images/female_placeholder_2.jpg",
+        email: "emily.johnson@hotmail.com",
         socials: [
-            {
-                icon: "L",
-                url: "https://www.linkedin.com",
-            },
-            {
-                icon: "T",
-                url: "https://www.twitter.com",
-            },
-            {
-                icon: "I",
-                url: "https://www.instagram.com",
-            },
+            { icon: "F", url: "https://www.facebook.com" },
+            { icon: "T", url: "https://www.twitter.com" },
+            { icon: "I", url: "https://www.instagram.com" },
         ],
     },
-
+    {
+        name: "Michael B. Lee",
+        role: "Office Manager",
+        image: "/images/male_placeholder_2.jpg",
+        email: "michael.lee@gmail.com",
+        socials: [
+            { icon: "F", url: "https://www.facebook.com" },
+            { icon: "T", url: "https://www.twitter.com" },
+            { icon: "I", url: "https://www.instagram.com" },
+        ],
+    },
+    {
+        name: "Sophia K. Martinez",
+        role: "Receptionist",
+        image: "/images/female_placeholder_3.jpg",
+        email: "sophia.martinez@yahoo.com",
+        socials: [
+            { icon: "F", url: "https://www.facebook.com" },
+            { icon: "T", url: "https://www.twitter.com" },
+            { icon: "I", url: "https://www.instagram.com" },
+        ],
+    },
+    {
+        name: "David A. Brown",
+        role: "Intern",
+        image: "/images/male_placeholder_3.jpg",
+        email: "david.brown@hotmail.com",
+        socials: [
+            { icon: "F", url: "https://www.facebook.com" },
+            { icon: "T", url: "https://www.twitter.com" },
+            { icon: "I", url: "https://www.instagram.com" },
+        ],
+    },
+    {
+        name: "Olivia M. Davis",
+        role: "Attorney",
+        image: "/images/female_placeholder_4.jpg",
+        email: "olivia.davis@gmail.com",
+        socials: [
+            { icon: "F", url: "https://www.facebook.com" },
+            { icon: "T", url: "https://www.twitter.com" },
+            { icon: "I", url: "https://www.instagram.com" },
+        ],
+    },
+    {
+        name: "James C. Wilson",
+        role: "Paralegal",
+        image: "/images/male_placeholder_4.jpg",
+        email: "james.wilson@yahoo.com",
+        socials: [
+            { icon: "F", url: "https://www.facebook.com" },
+            { icon: "T", url: "https://www.twitter.com" },
+            { icon: "I", url: "https://www.instagram.com" },
+        ],
+    },
+    {
+        name: "Isabella N. Moore",
+        role: "Legal Assistant",
+        image: "/images/female_placeholder_5.jpg",
+        email: "isabella.moore@hotmail.com",
+        socials: [
+            { icon: "F", url: "https://www.facebook.com" },
+            { icon: "T", url: "https://www.twitter.com" },
+            { icon: "I", url: "https://www.instagram.com" },
+        ],
+    },
+    {
+        name: "William D. Taylor",
+        role: "Office Manager",
+        image: "/images/male_placeholder_5.jpg",
+        email: "william.taylor@gmail.com",
+        socials: [
+            { icon: "F", url: "https://www.facebook.com" },
+            { icon: "T", url: "https://www.twitter.com" },
+            { icon: "I", url: "https://www.instagram.com" },
+        ],
+    },
+    {
+        name: "Mia P. Anderson",
+        role: "Receptionist",
+        image: "/images/female_placeholder_6.jpg",
+        email: "mia.anderson@yahoo.com",
+        socials: [
+            { icon: "F", url: "https://www.facebook.com" },
+            { icon: "T", url: "https://www.twitter.com" },
+            { icon: "I", url: "https://www.instagram.com" },
+        ],
+    },
+    {
+        name: "Alexander J. Thomas",
+        role: "Intern",
+        image: "/images/male_placeholder_6.jpg",
+        email: "alexander.thomas@hotmail.com",
+        socials: [
+            { icon: "F", url: "https://www.facebook.com" },
+            { icon: "T", url: "https://www.twitter.com" },
+            { icon: "I", url: "https://www.instagram.com" },
+        ],
+    },
+    {
+        name: "Charlotte R. Jackson",
+        role: "Attorney",
+        image: "/images/female_placeholder_7.jpg",
+        email: "charlotte.jackson@gmail.com",
+        socials: [
+            { icon: "F", url: "https://www.facebook.com" },
+            { icon: "T", url: "https://www.twitter.com" },
+            { icon: "I", url: "https://www.instagram.com" },
+        ],
+    },
+    {
+        name: "Benjamin K. White",
+        role: "Paralegal",
+        image: "/images/male_placeholder_7.jpg",
+        email: "benjamin.white@yahoo.com",
+        socials: [
+            { icon: "F", url: "https://www.facebook.com" },
+            { icon: "T", url: "https://www.twitter.com" },
+            { icon: "I", url: "https://www.instagram.com" },
+        ],
+    },
+    {
+        name: "Amelia S. Harris",
+        role: "Legal Assistant",
+        image: "/images/female_placeholder_8.jpg",
+        email: "amelia.harris@hotmail.com",
+        socials: [
+            { icon: "F", url: "https://www.facebook.com" },
+            { icon: "T", url: "https://www.twitter.com" },
+            { icon: "I", url: "https://www.instagram.com" },
+        ],
+    },
+    {
+        name: "Daniel L. Martin",
+        role: "Office Manager",
+        image: "/images/male_placeholder_8.jpg",
+        email: "daniel.martin@gmail.com",
+        socials: [
+            { icon: "F", url: "https://www.facebook.com" },
+            { icon: "T", url: "https://www.twitter.com" },
+            { icon: "I", url: "https://www.instagram.com" },
+        ],
+    },
+    {
+        name: "Evelyn T. Thompson",
+        role: "Receptionist",
+        image: "/images/female_placeholder_9.jpg",
+        email: "evelyn.thompson@yahoo.com",
+        socials: [
+            { icon: "F", url: "https://www.facebook.com" },
+            { icon: "T", url: "https://www.twitter.com" },
+            { icon: "I", url: "https://www.instagram.com" },
+        ],
+    },
+    {
+        name: "Matthew R. Garcia",
+        role: "Intern",
+        image: "/images/male_placeholder_9.jpg",
+        email: "matthew.garcia@hotmail.com",
+        socials: [
+            { icon: "F", url: "https://www.facebook.com" },
+            { icon: "T", url: "https://www.twitter.com" },
+            { icon: "I", url: "https://www.instagram.com" },
+        ],
+    },
+    {
+        name: "Abigail W. Martinez",
+        role: "Attorney",
+        image: "/images/female_placeholder_10.jpg",
+        email: "abigail.martinez@gmail.com",
+        socials: [
+            { icon: "F", url: "https://www.facebook.com" },
+            { icon: "T", url: "https://www.twitter.com" },
+            { icon: "I", url: "https://www.instagram.com" },
+        ],
+    },
+    {
+        name: "Christopher N. Robinson",
+        role: "Paralegal",
+        image: "/images/male_placeholder_10.jpg",
+        email: "christopher.robinson@yahoo.com",
+        socials: [
+            { icon: "F", url: "https://www.facebook.com" },
+            { icon: "T", url: "https://www.twitter.com" },
+            { icon: "I", url: "https://www.instagram.com" },
+        ],
+    },
 ];
+
